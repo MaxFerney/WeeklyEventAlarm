f3a5e0a159aa714affb2107ec5ce176f83834132
MaxFerney <ferneym2@winthrop.edu>
2021-03-09 13:09:40 -0500

    geolocation needs workin

--- calendar-alarm/imports/ui/pages/Home.jsx
+++ calendar-alarm/imports/ui/pages/Home.jsx
@@ -2,12 +2,28 @@
 import React, { useState } from 'react';
 import {NavLink, Link} from "react-router-dom";
 import PropTypes from 'prop-types';
+//import {geolocation} from 'cordova-plugin-geolocation@0.3.12'
 // import {  } from "react-router";
 
 // Components
 // import Header from './../components/Header.js';
 // import Footer from './../components/Footer.js';
-
+function successCallback(){
+    console.log('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
+function onError(error) {
+alert('code: '    + error.code    + '\n' +
+    'message: ' + error.message + '\n');
+}
+navigator.geolocation.getCurrentPosition(successCallback,
+                                         [onError]);
 const HomePage = (props) => {
 
   function getDay(){
