4cdc0fb94cd66926f49c9dffb2eb7d62abd094fb
MaxFerney <ferneym2@winthrop.edu>
2021-03-09 13:52:31 -0500

    used momentjs to make pretty time look pretty

--- calendar-alarm/imports/ui/pages/Home.jsx
+++ calendar-alarm/imports/ui/pages/Home.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import {NavLink, Link, Redirect} from "react-router-dom";
 import PropTypes from 'prop-types';
+import moment from 'moment';
 // import {  } from "react-router";
 
 // Components
@@ -9,103 +10,12 @@ import PropTypes from 'prop-types';
 // import Footer from './../components/Footer.js';
 
 const HomePage = (props) => {
-
-  function getDay(){
-    let date = new Date();
-    let day=date.getDay();
-    switch (day){
-        case 1:{
-            day="Monday";
-            break;
-        }
-        case 2:{
-            day="Tuesday";
-            break;
-        }
-        case 3:{
-            day="Wednesday";
-            break;
-        }
-        case 4:{
-            day="Thursday";
-            break;
-        }
-        case 5:{
-            day="Friday";
-            break;
-        }
-        case 6:{
-            day="Saturday";
-            break;
-        }
-        case 0:{
-            day="Sunday";
-            break;
-        }
-    }
-
-    return day;
-  }
-  function getDate(){
-      let date = new Date();
-      let dateOfMonth=date.getDate();
-      return dateOfMonth;
-  }
-  function getMonth(){
-      let date = new Date();
-      let month=date.getMonth();
-      switch(month){
-          case 0:{
-              month="January";
-              break;
-          }
-          case 1:{
-              month="Febuary";
-              break;
-          }
-          case 2:{
-              month="March";
-              break;
-          }
-          case 3:{
-              month="April";
-              break;
-          }
-          case 4:{
-              month="May";
-              break;
-          }
-          case 5:{
-              month="June";
-              break;
-          }
-          case 6:{
-              month="July";
-              break;
-          }
-          case 7:{
-              month="August";
-              break;
-          }
-          case 8:{
-              month="September";
-              break;
-          }
-          case 9:{
-              month="October";
-              break;
-          }
-          case 10:{
-              month="November";
-              break;
-          }
-          case 11:{
-              month="December";
-              break;
-          }
-      }
-      return month;
-  }
+    const getUnixSec = () =>{
+        const ESTCurrentTimeFix = 18000000;
+        let date = new Date();
+        return (date.getTime()+ESTCurrentTimeFix);
+    };
+    
     const redirectToCal=()=>{
         return(
             <Redirect to={{
@@ -119,7 +29,7 @@ const HomePage = (props) => {
   //Needs to route to Calendar.jsx
     return(
         <div id="homePage">
-            <h1>{getDay()},{getMonth()} {getDate()}</h1>
+            <h1>{moment(getUnixSec()).format('dddd, MMMM Do')}</h1>
             <div id="dailyEvents">
                 <h2>Today's Events</h2>
                 <ul>
